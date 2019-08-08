05b654c49cee2dbb82e2292d4f1205dcc27e71f1
Jacob Rosenzweig <rosenjcb@gmail.com>
2019-08-07 17:00:56 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Party Finder
+
+## Frontend
+pf-react is the frontend.
+
+## Backend
+pf-spring will be the backend.
---
+++ pf-react/src/home/Home.js
@@ -0,0 +1,53 @@
+import React, { Component }  from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import "./Home.css";    
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+          collapsed: true
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+          collapsed: !this.state.collapsed
+        });
+    }
+
+    render() {
+        return (
+            <div className="homepage">
+            <Navbar color="faded" dark>
+                <NavbarBrand href="/" className="mr-auto">Party Finder</NavbarBrand>
+                <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                <Collapse isOpen={!this.state.collapsed} navbar>
+                <Nav navbar>
+                    <NavItem>
+                        <NavLink href="/search">Find a Party</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/create">Create a Party</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/signup">Sign Up</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/login">Login</NavLink>
+                    </NavItem>
+                </Nav>
+                </Collapse>
+            </Navbar>
+            <h1 className="left">Creating and joining startups is a difficult task. As of now, there is no single place that a person can go to find the right team or the right talent.</h1>
+            <div className="collab"></div>
+            <h2 className="right">Party Finder is a platform for startups. Find your party and begin your quest. What are you waiting for?</h2>
+            <div className="left app"></div>
+            </div>
+        );
+    }
+}
+
+export default Home;
